201433c28038137f13709b8b7f22c293ea3cf476
voltarka <70635056+voltarka@users.noreply.github.com>
2020-09-22 20:20:59 +0300

    Update lr1var4n5.cpp

--- lr1var4n5.cpp
+++ lr1var4n5.cpp
@@ -7,7 +7,7 @@ int main(){
    unsigned short int d;
    unsigned short int e;
    unsigned short int f;
-   int x ;
+   unsigned short int x ;
    cout<< "Здравствуйте! Введите логические значения следующих переменных:"<< endl;
    cout<< "c - ";
    cin >> c;
@@ -17,7 +17,7 @@ int main(){
    cin >> e;
    cout<< "f - ";
    cin >> f;
-   x = (c&e)| ~d ^ f;
-   cout<< "(c /\ e) ^ !d+f ="<< showbase<< hex<< setw(2) << x<< endl;
+   x = (c&e)| (~d) ^ f;
+   cout<< "(c /\ e) ^ !d+f ="<< showbase<< hex<< setw(2) << x<<internal<< endl;
     return 0;
 }
