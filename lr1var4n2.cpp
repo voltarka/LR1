6dc63c5b2abe75cd7dc452e79735cf12df69d425
voltarka <70635056+voltarka@users.noreply.github.com>
2020-10-20 14:00:57 +0300

    Update lr1var4n2.cpp

--- lr1var4n2.cpp
+++ lr1var4n2.cpp
@@ -1,15 +1,12 @@
+
 #include <iostream>
 #include <cmath>
-#include <iomanip>
-using namespace std;
 int main (){
-    unsigned int a;
-cout<< "Hello! Please, enter a - ";
-cin >> a;
-a= 5* abs(float(a))- sqrt(3*a - 1);
-cout << "5|a|-sqrt(3a-1) = "<< showpos<<a<<"     ";
-
-
+   unsigned int a;
+   float x = 0;
+    std::cout<< "Please, enter a from 0 to 2^32-1 : "<< std::endl;
+    std::cin >> a;
+x = 5* abs(a)- sqrt(3*a - 1);
+std::cout << "5|a|-sqrt(3a-1) = "<< std::showpos<<x<<std::endl;
     return 0;
-
 }
